da7a4c9167b7eef08f931663560bf776dc430be3
hkumarmk <hkumarmk@gmail.com>
2014-11-26 17:21:48 +0530

    Radosgw work and refined some stuffs in ceph
    1. Made radosgw working with latest versions
    2. Refined ceph::auth
    3. Removed http config template and used apache::vhost instead

--- manifests/radosgw.pp
+++ manifests/radosgw.pp
@@ -10,63 +10,69 @@
 #
 # == Authors
 #
-#  Harish Kumar	harish.m.kumar@ril.com
+#  Harish Kumar <hkumar@d4devops.org>
 #
+# TODO:
+# Ceph patched packages for apache and fastcgi is there in ceph repos. Those
+# repos to be added in the module itself.
+# For some communication, keystone use signed messages which need keystone
+# certificate to be added in ceph radosgw and added as nssdb format. This is to
+# be done. Due to this, radosgw will not get updates on revoked tokens, also
+# there will be few errors printed during restart of radosgw. This will be added
+# as soon as the basic stuffs working.
 #
 #
 class ceph::radosgw (
   $keystone_url,
   $keystone_admin_token,
-  $keystone_accepted_roles = 'Member, admin, swiftoperator',
-  $keystone_token_cache_size = 500,
+  $mon_key,
+  $keystone_accepted_roles      = '_member_, admin, swiftoperator',
+  $keystone_token_cache_size    = 500,
   $keystone_revocation_interval = 600,
-  $nss_db_path = '/var/lib/ceph/nss',
-  $package_ensure = 'present',
-  $configure_apache        = true,
-  $bind_address            = '0.0.0.0',
-  
-  $serveradmin_email    = 'root@localhost',
-  $fastcgi_ext_script   = '/var/www/s3gw.fcgi',
-  $socket   = '/var/run/ceph/radosgw.sock',
-  $fastcgi_ext_script_source = undef,
-
-  $listen_ssl   = false,
-  $radosgw_cert_file_source     = undef,
-  $radosgw_cert_file = undef,
-  $radosgw_key_file  = undef,
-  $radosgw_key_file_source  = undef,
-  $radosgw_ca_file   = undef,
-  $radosgw_ca_file_source   = undef,
-  $logfile		= '/var/log/ceph/radosgw',
-  $keyring	= '/etc/ceph/keyring',   
-  $radosgw_keyring = '/etc/ceph/keyring.radosgw.gateway',
-  $region            = 'RegionOne',
-  $public_protocol   = 'http',
-  $public_address    = '127.0.0.1',
-  $public_port       = undef,
-  $admin_protocol    = 'http',
-  $admin_address     = undef,
-  $internal_protocol = 'http',
-  $internal_address  = undef,
-  $radosgw_apache_version = '2.2',
+  $nss_db_path                  = '/var/lib/ceph/nss',
+  $package_ensure               = 'present',
+  $configure_apache             = true,
+  $bind_address                 = '0.0.0.0',
+  $fastcgi_ext_script           = '/var/www/s3gw.fcgi',
+  $socket                       = '/var/run/ceph/radosgw.sock',
+  $listen_ssl                   = false,
+  $radosgw_cert_file            = undef,
+  $radosgw_key_file             = undef,
+  $radosgw_ca_file              = undef,
+  $logfile                      = '/var/log/ceph/radosgw',
+  $keyring                      = '/etc/ceph/keyring',
+  $radosgw_keyring              = undef,
+  $region                       = 'RegionOne',
+  $radosgw_id                   = 'radosgw.gateway',
+  $gw_serveradmin_email         = 'root@localhost',
+  $gw_server_name               = 'localhost',
 ) {
 
-  Exec['ceph-radosgw-key'] ~> Service['radosgw']
+  if ! $radosgw_keyring {
+    $radosgw_keyring_orig = "/etc/ceph/keyring.${radosgw_id}"
+  } else {
+    $radosgw_keyring_orig = $radosgw_keyring
+  }
 
-  #include 'ceph::conf'
+  ##
+  # refreshing the service on changing the configurations,
+  # Ideally we should have ceph_config type to configure lot of this.
+  ##
+
+  Ceph::Auth[$radosgw_id] ~> Service['radosgw']
+  Concat['/etc/ceph/ceph.conf'] ~> Service['radosgw']
 
   ceph::conf::radosgw { $name:
-    keyring  	=> $radosgw_keyring,
-    socket 	=> $socket,
-    logfile	=> $logfile,
-    keystone_url => $keystone_url,
-    keystone_admin_token => $keystone_admin_token,
-    keystone_accepted_roles => $keystone_accepted_roles,
-    keystone_token_cache_size => $keystone_token_cache_size,
+    keyring                      => $radosgw_keyring_orig,
+    socket                       => $socket,
+    logfile                      => $logfile,
+    keystone_url                 => $keystone_url,
+    keystone_admin_token         => $keystone_admin_token,
+    keystone_accepted_roles      => $keystone_accepted_roles,
+    keystone_token_cache_size    => $keystone_token_cache_size,
     keystone_revocation_interval => $keystone_revocation_interval,
-    nss_db_path => $nss_db_path,
+    nss_db_path                  => $nss_db_path,
   }
-  
 
   package { 'radosgw':
     ensure => $package_ensure
@@ -74,40 +80,45 @@ class ceph::radosgw (
 
   if $configure_apache {
     class { 'ceph::radosgw::apache':
-      bind_address => $bind_address,
-      listen_ssl   => $listen_ssl,
-      radosgw_cert_file => $radosgw_cert_file,
-      radosgw_key_file  => $radosgw_key_file,
-      radosgw_ca_file   => $radosgw_ca_file,
-      radosgw_cert_file_source => $radosgw_cert_file_source,
-      radosgw_key_file_source  => $radosgw_key_file_source,
-      radosgw_ca_file_source   => $radosgw_ca_file_source,
-      serveradmin_email 	=> $serveradmin_email,
-      fastcgi_ext_script	=> $fastcgi_ext_script,
-      fastcgi_ext_socket	=> $socket,
-      fastcgi_ext_script_source	=> $fastcgi_ext_script_source,
-      radosgw_apache_version	=> $radosgw_apache_version,
+      bind_address       => $bind_address,
+      listen_ssl         => $listen_ssl,
+      radosgw_cert_file  => $radosgw_cert_file,
+      radosgw_key_file   => $radosgw_key_file,
+      radosgw_ca_file    => $radosgw_ca_file,
+      fastcgi_ext_script => $fastcgi_ext_script,
+      fastcgi_ext_socket => $socket,
+      serveradmin_email  => $gw_serveradmin_email,
+      server_name        => $gw_server_name,
     }
   }
 
-exec { 'ceph-radosgw-key':
-    command => "ceph-authtool --create-keyring $radosgw_keyring; ceph-authtool $radosgw_keyring \
---name=client.radosgw.gateway \
---add-key \
-$(ceph --name client.admin --keyring $keyring \
-  auth get-or-create-key client.radosgw.gateway \
-    mon 'allow rw' \
-    osd 'allow rwx' ); chmod a+r $radosgw_keyring",
-    require => Package['radosgw'],
-    unless  => "egrep 'client.radosgw.gateway' $radosgw_keyring",
+  ##
+  # Add cephx entry for radosgw.
+  ##
+
+  ceph::auth {$radosgw_id:
+    mon_key      => $mon_key,
+    client       => $radosgw_id,
+    keyring_path => $radosgw_keyring_orig,
+    cap          => "mon 'allow rw' osd 'allow rwx'",
+  }
+
+  ##
+  # Create radosgw data directory
+  ##
+
+  file {"/var/lib/ceph/radosgw/${radosgw_id}":
+    ensure => 'directory',
   }
 
   service { 'radosgw':
-	ensure     => 'running',
-	enable	   => true,
-	hasstatus => true,
-	provider  => "init",
-	hasrestart => true,
+    ensure     => 'running',
+    enable     => true,
+    provider   => 'init',
+    hasstatus  => true,
+    hasrestart => true,
+    require    => [Ceph::Conf::Radosgw[$name],
+                    File["/var/lib/ceph/radosgw/${radosgw_id}"]],
   }
 
 }
--- manifests/radosgw/apache.pp
+++ manifests/radosgw/apache.pp
@@ -20,76 +20,57 @@
 #    (required with listen_ssl) CA certificate to use for SSL support.
 #
 class ceph::radosgw::apache (
-  $bind_address = '0.0.0.0',
-  $fqdn		= 'localhost',
-  $serveradmin_email	= 'root@localhost',
-  $fastcgi_ext_script	= '/var/www/s3gw.fcgi',
-  $fastcgi_ext_socket	= '/var/run/ceph/radosgw.sock',
-  $fastcgi_ext_script_source = undef,
-  
-  $listen_ssl   = false,
-  $radosgw_cert_file_source	= undef,
-  $radosgw_cert_file = undef,
-  $radosgw_key_file  = undef,
-  $radosgw_key_file_source  = undef,
-  $radosgw_ca_file   = undef,
-  $radosgw_ca_file_source   = undef,
-  $radosgw_apache_version = '2.2',
+  $bind_address           = '0.0.0.0',
+  $serveradmin_email      = 'root@localhost',
+  $server_name            = 'localhost',
+  $fastcgi_ext_script     = '/var/www/s3gw.fcgi',
+  $fastcgi_ext_socket     = '/var/run/ceph/radosgw.sock',
+  $listen_port_http       = 80,
+  $listen_port_https      = 443,
+  $listen_ssl             = false,
+  $radosgw_cert_file      = undef,
+  $radosgw_key_file       = undef,
+  $radosgw_ca_file        = undef,
+  $docroot                = '/var/www',
 ) {
 
   include ceph::radosgw::params
-  apt::pin { 'ceph_packages':
-    priority => '900',
-    packages => '*apache2*',
-    originator => 'InkTank',
-    ensure => present,
-  }
-  ->
-  class {'::apache':
-    apache_version => $radosgw_apache_version,
-  }
+  include ::apache
   include ::apache::mod::fastcgi
-  include apache::mod::rewrite
+  include ::apache::mod::rewrite
 
 
   Package['radosgw'] -> Package[$::ceph::radosgw::params::http_service]
-  File[$::ceph::radosgw::params::httpd_config_file] ~> Service[$::ceph::radosgw::params::http_service]
-  
-#  package { $radosgw_apache_deps:
-#    ensure => $radosgw_apache_version,
-#  }
 
- # file { $log_dir:
- #   ensure  => directory,
- #   owner   => $::ceph::radosgw::params::apache_user,
- #   group   => $::ceph::radosgw::params::apache_group,
- #   before  => Service[$::ceph::radosgw::params::http_service],
- #   mode    => '0751',
- #   require => Package['radosgw']
- # }
 
-    file { $fastcgi_ext_script:
-        ensure        => file,
-        owner         => root,
-        group         => $::ceph::radosgw::params::apache_group,
-        mode          => 750,
-	content	  	=> "#!/bin/sh\nexec /usr/bin/radosgw -c /etc/ceph/ceph.conf -n client.radosgw.gateway",
-        before  => File[$::ceph::radosgw::params::httpd_config_file],
-	notify  => Service['httpd'],
-    }
-
-  file { $::ceph::radosgw::params::httpd_config_file:
+  file { $fastcgi_ext_script:
     ensure  => file,
-    owner   => $::ceph::radosgw::params::apache_user,
+    owner   => root,
     group   => $::ceph::radosgw::params::apache_group,
-    content => template('ceph/radosgw-apache.conf.erb'),
-    before  => Service[$::ceph::radosgw::params::http_service],
-    mode    => '0644',
-    require => Package['radosgw'],
+    mode    => '0750',
+    content => "#!/bin/sh\nexec /usr/bin/radosgw -c /etc/ceph/ceph.conf -n client.radosgw.gateway",
     notify  => Service['httpd'],
   }
-  include apache::mod::proxy
-  include apache::mod::proxy_http
+
+  apache::vhost { 'radosgw':
+    servername            => $server_name,
+    serveradmin           => $serveradmin_email,
+    docroot               => $docroot,
+    port                  => $listen_port_http,
+    ssl                   => false,
+    error_log_file        => 'radosgw_error.log',
+    access_log_file       => 'radosgw.log',
+    fastcgi_server        => $fastcgi_ext_script,
+    fastcgi_socket        => $fastcgi_ext_socket,
+    fastcgi_dir           => $docroot,
+    allow_encoded_slashes => 'on',
+    rewrites              =>  [{
+                                rewrite_rule => ['^/(.*) /s3gw.fcgi?%{QUERY_STRING} [E=HTTP_AUTHORIZATION:%{HTTP:Authorization},L]']
+                              }],
+    require               => [Package['radosgw'],
+                              File[$fastcgi_ext_script]],
+  }
+
   if $listen_ssl {
     include apache::mod::ssl
 
